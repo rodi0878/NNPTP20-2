c9f16c8016026ebe63af6267dbf8982033b95d0d
xPeep <44339484+xPeep@users.noreply.github.com>
2020-10-12 20:11:18 +0200

    Refactor method findByEntryByTitle
    Modern solution which is easy to read.

--- src/main/java/cz/upce/fei/inptp/zz/entity/PasswordDatabase.java
+++ src/main/java/cz/upce/fei/inptp/zz/entity/PasswordDatabase.java
@@ -7,47 +7,44 @@ package cz.upce.fei.inptp.zz.entity;
 
 import java.io.File;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
 
 /**
- *
  * @author Roman
  */
 public class PasswordDatabase {
     private File file;
     private String passwd;
-    
+
     private List<Password> passwords;
 
     public PasswordDatabase(File file, String passwd) {
         this.file = file;
         this.passwd = passwd;
     }
-    
+
     public void load() {
         // TODO: use JSON and CryptoFile to load
         // TODO: throw exceptions when error
     }
-    
+
     public void save() {
         // TODO: use JSON and CryptoFile t save
     }
-    
+
     public void add(Password password) {
         passwords.add(password);
     }
-    
+
     public Password findEntryByTitle(String title) {
-        for (Password password : passwords) {
-            
-            if (password.hasParameter(Parameter.StandardizedParameters.TITLE)) {
-                Parameter.TextParameter titleParam;
-                titleParam = (Parameter.TextParameter)password.getParameter(Parameter.StandardizedParameters.TITLE);
-                if (titleParam.getValue().equals(titleParam)) {
-                    return password;
-                }
-            }
-        }
-        return null;
+        return passwords
+                .stream()
+                .filter(password ->
+                        password.hasParameter(Parameter.StandardizedParameters.TITLE)
+                                && Objects.equals(((Parameter.TextParameter) password.getParameter(Parameter.StandardizedParameters.TITLE)).getValue(), title)
+                )
+                .findFirst()
+                .orElse(null);
     }
-    
 }
