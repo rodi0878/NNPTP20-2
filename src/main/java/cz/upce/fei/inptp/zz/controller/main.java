2773d409e95eb003d01675b6a3316305fdf59677
Michal Černota <st52494@upce.cz>
2020-11-28 14:30:31 +0100

    Main refactoring

--- src/main/java/cz/upce/fei/inptp/zz/controller/main.java
+++ src/main/java/cz/upce/fei/inptp/zz/controller/main.java
@@ -2,9 +2,7 @@ package cz.upce.fei.inptp.zz.controller;
 
 import cz.upce.fei.inptp.zz.entity.PasswordDatabase;
 import cz.upce.fei.inptp.zz.injector.InstanceInjector;
-import cz.upce.fei.inptp.zz.service.json.JSONFileService;
 import cz.upce.fei.inptp.zz.entity.Password;
-import cz.upce.fei.inptp.zz.service.password.JSONPasswordDatabaseService;
 import cz.upce.fei.inptp.zz.service.password.PasswordDatabaseService;
 
 import java.io.File;
@@ -13,29 +11,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Enterprise password manager - in console
- * - uses strong industry-based encryption algorithm
- * - stores your passwords and relevent information
- * - allows you to simply manage all stored informations
- * 
- * 
+ * Enterprise password manager - in console - uses strong industry-based
+ * encryption algorithm - stores your passwords and relevent information -
+ * allows you to simply manage all stored informations
+ *
+ *
  */
 public class main {
+
     public static void main(String[] args) {
-        List<Password> pwds = new ArrayList<>();
-        pwds.add(new Password(0, "sdfghjkl"));
-        pwds.add(new Password(1, "ASDSAFafasdasdasdas"));
-        pwds.add(new Password(2, "aaa-aaaa-"));
+        List<Password> passwordsList = new ArrayList<>();
+        passwordsList.add(new Password(0, "sdfghjkl"));
+        passwordsList.add(new Password(1, "ASDSAFafasdasdasdas"));
+        passwordsList.add(new Password(2, "aaa-aaaa-"));
 
         PasswordDatabaseService databaseService = InstanceInjector.injector().getInstance(PasswordDatabaseService.class);
-        databaseService.savePasswordDatabase(new PasswordDatabase(new File("test.txt"), "password", pwds));
+        databaseService.savePasswordDatabase(new PasswordDatabase(new File("test.txt"), "password", passwordsList));
 
         try {
             String read = databaseService.openPasswordDatabase(new File("test.txt"), "password").getPassword();
             System.out.println(read);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            e.printStackTrace(System.out);
         }
     }
-   
 }
